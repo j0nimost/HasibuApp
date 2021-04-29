7009b232611e2c5f399bd08d5adaff5f35df80ff
John Nyingi <JNyingi@resolution.lan>
2021-04-29 16:23:32 +0300

    Added Feature to fetch a specific account record

--- src/api-routes/accountRoute.js
+++ src/api-routes/accountRoute.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.route('/store/:storeId')
         .get(accountController.getstoreAccountAsync);
 
-router.route('/:id')
+router.route('/:accountRef')
         .get(accountController.getAccountAsync);
 
 module.exports = router;
